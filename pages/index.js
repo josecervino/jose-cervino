5495faf7e1e036f7afbcd9b2e69ab3f88155e91c
José Cerviño <jciv.public@gmail.com>
2023-02-09 17:00:19 -0500

    update: subheader copy

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <>
           <h1 className="text-3xl pb-2">José Cerviño IV</h1>
-          <h2>Fractional CTO, CEO and SMB Investor</h2>
+          <h2>Fractional CTO, SMB Investor & CEO</h2>
         </>
         <a className="mt-[30%] hover:text-slate-500 underline" href="mailto:contact@josecervino.com">Schedule an AI & Tech consult</a>
       </main>
